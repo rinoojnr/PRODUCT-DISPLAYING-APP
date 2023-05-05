6cb84f9c00015ea5c62007be2870bc69c6c8377e
Rinooj N R <rinoojnr@gmail.com>
2023-05-05 19:23:57 +0530

    modified navbar and product structure

--- product-app/src/App.js
+++ product-app/src/App.js
@@ -1,12 +1,12 @@
 import Header from './Components/Header/Header';
 import './App.css';
-import Footer from './Components/Footer/Footer';
+import Body from './Components/Body/Body';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Footer />
+      <Body />
     </div>
   );
 }
